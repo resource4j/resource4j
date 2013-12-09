d1241ace5745af9113c166d2b1f46356a458494e
Ivan Gammel <igammel@aligntech.com>
2013-12-09 23:57:23 +0400

    started implementing Spring integration example

--- examples/server/src/main/java/com/github/resource4j/examples/web/controller/HomeController.java
+++ examples/server/src/main/java/com/github/resource4j/examples/web/controller/HomeController.java
@@ -1,18 +1,19 @@
-package com.github.resource4j;
+package com.github.resource4j.examples.web.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.github.resource4j.spring.ResourceValue;
+import com.github.resource4j.examples.domain.model.WeatherEnum;
+import com.github.resource4j.examples.domain.service.Clock;
+import com.github.resource4j.examples.domain.service.WeatherService;
 
 /**
  * Handles requests for the application home page.
@@ -22,8 +23,11 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@ResourceValue
-	private String message;
+	@Inject
+	private Clock clock;
+	
+	@Inject
+	private WeatherService weatherService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -31,13 +35,10 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+
+		WeatherEnum weather = weatherService.getCurrentWeather();
+		model.addAttribute("currentDate", clock.today());
+		model.addAttribute("weather", weather);
 		
 		return "home";
 	}
