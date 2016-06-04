2365b9b038010d9755044ed246856cf41fea882a
Ivan Gammel <ivan-gammel@users.noreply.github.com>
2016-06-04 21:04:56 +0300

    #16 - added unit test for java time durations

---
+++ converters/src/test/java/com/github/resource4j/converters/DurationToStringTest.java
@@ -0,0 +1,43 @@
+package com.github.resource4j.converters;
+
+import org.junit.Test;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+public class DurationToStringTest {
+
+    public static final Duration SOME_LONG_DURATION = Duration.ofDays(3).plusHours(5).plusMinutes(30);
+    public static final String SOME_LONG_DURATION_STRING = "PT77H30M";
+    public static final String SOME_LONG_NEGATIVE_DURATION_STRING = "PT-77H-30M";
+
+    @Test
+    public void testDurationToStringConversion() {
+        Duration duration = SOME_LONG_DURATION;
+        String actual = TypeConverter.convert(duration, String.class);
+        assertEquals(SOME_LONG_DURATION_STRING, actual);
+    }
+
+    @Test
+    public void testStringToDurationConversion() {
+        Duration expected = SOME_LONG_DURATION;
+        Duration actual = TypeConverter.convert(SOME_LONG_DURATION_STRING, Duration.class);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNegativeDurationToStringConversion() {
+        Duration duration = SOME_LONG_DURATION.negated();
+        String actual = TypeConverter.convert(duration, String.class);
+        assertEquals("-" + SOME_LONG_DURATION_STRING, actual);
+    }
+
+    @Test
+    public void testStringToNegativeDurationConversion() {
+        Duration expected = SOME_LONG_DURATION.negated();
+        Duration actual = TypeConverter.convert(SOME_LONG_DURATION_STRING, Duration.class);
+        assertEquals(expected, actual);
+    }
+
+}
